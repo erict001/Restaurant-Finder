2f0a4dc36c08372b901ae86d532617c2ff983d7d
Taylor Shen <shen.taylor@yahoo.com>
2022-05-10 12:12:16 -0700

    server.js update

--- server.js
+++ server.js
@@ -9,8 +9,27 @@ const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+const sess = {
+  secret: process.env.SESSION_SECRET,
+  cookie: {
+      maxAge:1000*60*60*2
+  },
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+      db: sequelize
+  })
+};
+
+app.use(session(sess));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static('public'));
+
+const hbs = exphbs.create({});
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 app.use(routes);
 
