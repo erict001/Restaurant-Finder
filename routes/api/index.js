fa220ab18323a0e813ccc708149a42cb77ae7d36
Interrubble <delagarzachris@icloud.com>
2022-05-10 11:59:36 -0700

    added userRoutes and updated frontEndRoutes

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const router = require('express').Router();
 
+const userRoutes = require('./userRoutes');
+router.use('/user', userRoutes);
+
 const frontEndRoutes = require('./frontEndRoutes');
 router.use('/', frontEndRoutes);
 
