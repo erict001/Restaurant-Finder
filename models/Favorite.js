e9505d06a9fee82757d57f8d31f00477348f3dc2
Interrubble <delagarzachris@icloud.com>
2022-05-11 10:16:28 -0700

    wut x2

---
+++ models/Favorite.js
@@ -0,0 +1,38 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+
+class Favorite extends Model {}
+
+Favorite.init({
+    id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+    },
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    location: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    user_id: {
+        type: DataTypes.INTEGER,
+        allowNull:false,
+        references: {
+            model:'user',
+            key:'id'
+        }
+    }
+},{
+    sequelize,
+    timestamps:false,
+    freezeTableName:true,
+    underscored:true,
+    modelName:'favorites'
+});
+
+module.exports = Favorite
