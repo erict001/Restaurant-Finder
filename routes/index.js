30789e64f5a0eeebba3dd2917a5a84806f973d6c
Eric Taylor <eric.taylor1012@gmail.com>
2022-05-12 10:37:46 -0700

    api calls looped

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,14 @@
 const router = require('express').Router();
 const apiRoutes = require('./api');
 const frontEndRoutes = require('./frontEndRoutes');
+const restaurantRoutes = require('./api/restaurantRoutes')
 
 router.use('/api', apiRoutes);
 
 router.use('/', frontEndRoutes);
+
+router.use('/restaurant', restaurantRoutes);
+
 
 router.use((req, res) => {
   res.send("<h1>Wrong Route!</h1>")
