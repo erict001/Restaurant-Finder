669088a25a7553671d76161338d17fa7b088a2b5
Interrubble <delagarzachris@icloud.com>
2022-05-12 12:29:05 -0700

    acquired data from api

--- routes/api/restaurantRoutes.js
+++ routes/api/restaurantRoutes.js
@@ -1,45 +1,65 @@
 const express = require('express');
 const router = express.Router();
-const {User, Favorite} = require('../../models')
+const { User, Favorite } = require('../../models')
 
 const apiKey = "dyKoMIant5tA4GF_vX1UaxJLb-TfUwZYCtl0VRWMALgH7lh844ReqqLxQoEvbwuxVWa5L20BHtg0jFKVYo3dQ_TJbqUQuJ8DmB2oaj6ACsn8ctez8syWn2tAU7R6YnYx";
 
 const yelp = require('yelp-fusion');
 const client = yelp.client(apiKey);
 
+router.get('/', (req, res) => {
+    console.log("I'm here")
+    const businessArray = []
+    client.search({
+        term: req.body.busName,
+        location: req.body.busLocal,
+    }).then(response => {
+        console.log(response.jsonBody.businesses[0])
+        for (var i = 0; i < 3; i++) {
+            businessArray.push(
+                response.jsonBody.businesses[i]
+            )
+        }
+        return res.render("profile",businessArray)
+        // return res.json([termality, locality]);
+        // res.render("profile", 
+    }).catch(e => {
+        console.log(e);
+    });
+});
 
-// router.post('/', (req, res) => {
-//     const businessArray = []
-//     client.search({
-//        term: 'pizza',
-//        location: 'Seattle',
-//    }).then(response => {
-//        for (var i = 0; i < 3 ; i++) {
-//            businessArray.push(
-//                response.jsonBody.businesses[i]
-//            )
-//         }
-//         return res.render("profile", businessArray)
-//         // return res.json([termality, locality]);
-//        // res.render("profile", 
-//    }).catch(e => {
-//        console.log(e);
-//    });
-// });
+router.post('/', (req, res) => {
+    console.log("this is post route====================================")
+    const businessArray = []
+    client.search({
+        term: req.body.busName,
+        location: req.body.busLocal,
+    }).then(response => {
+        for (var i = 0; i < 3; i++) {
+            businessArray.push(
+                response.jsonBody.businesses[i]
+                )
+                console.log(response.jsonBody.businesses[i])
+        }
+        return res.json(businessArray)
+    }).catch(e => {
+        console.log(e);
+    });
+});
 
-router.get("/favorites",(req,res)=>{
-    if(!req.session.user){
+router.get("/favorites", (req, res) => {
+    if (!req.session.user) {
         return res.redirect("/login")
     }
-    Favorite.findByPk(req.session.user.id,{
-        include:[Favorite]
-    }).then(favoritesData=>{
+    Favorite.findByPk(req.session.user.id, {
+        include: [Favorite]
+    }).then(favoritesData => {
         console.log(favoritesData);
-        const hbsData = favoritesData.get({plain:true})
+        const hbsData = favoritesData.get({ plain: true })
         console.log("=======")
         console.log(hbsData);
-        hbsData.loggedIn = req.session.user?true:false
-        res.render("profile",hbsData)
+        hbsData.loggedIn = req.session.user ? true : false
+        res.render("profile", hbsData)
     })
 })
 
